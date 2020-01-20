c0b8e9f50254f58bee6de0d7ac93c9bb337371e9
Cory <ccrowley96@gmail.com>
2020-01-20 11:46:42 -0800

    Updates

---
+++ precache-manifest.631b6864dd55fa5676d94722809844ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3761d56de6a925211a28f9c4b522a372",
+    "url": "/portfolio-playground/index.html"
+  },
+  {
+    "revision": "21657b8c17f20359e6ec",
+    "url": "/portfolio-playground/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1a05ead8f7ec76c783b9",
+    "url": "/portfolio-playground/static/js/2.87d661a7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio-playground/static/js/2.87d661a7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "21657b8c17f20359e6ec",
+    "url": "/portfolio-playground/static/js/main.fa28bcd2.chunk.js"
+  },
+  {
+    "revision": "7cbfb5543812468beb78",
+    "url": "/portfolio-playground/static/js/runtime-main.597f702b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-playground/static/media/logo.5d5d9eef.svg"
+  }
+]);
